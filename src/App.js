670ddd242d389ed198846047e6f1267ef83ac621
KhoaDang <dangkhoa1410@gmail.com>
2021-05-28 15:50:13 -0400

    Projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About/About';
 import Banner from './components/Banner/Banner';
 import Nav from './components/Nav/Nav';
+import Projects from './components/Projects/Projects';
 import Summary from './components/Summary/Summary';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Banner></Banner>
       <About></About>
       <Summary></Summary>
+      <Projects></Projects>
     </div>
   );
 }
