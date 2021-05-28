feb41c4648b4edf441d4c5f7eaebe52ce5b74cd2
KhoaDang <dangkhoa1410@gmail.com>
2021-05-28 15:43:39 -0400

    Summary

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About/About';
 import Banner from './components/Banner/Banner';
 import Nav from './components/Nav/Nav';
+import Summary from './components/Summary/Summary';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav></Nav>
       <Banner></Banner>
       <About></About>
+      <Summary></Summary>
     </div>
   );
 }
