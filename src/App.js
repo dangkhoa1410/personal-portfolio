6f75aedf8e164a98a4fd401fbbf726533f7d1934
KhoaDang <dangkhoa1410@gmail.com>
2021-05-28 14:18:48 -0400

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './components/Nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
