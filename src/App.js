8651c6625cb21523813f994f64ddafc8e3174df2
KhoaDang <dangkhoa1410@gmail.com>
2021-05-28 15:16:17 -0400

    Banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Banner from './components/Banner/Banner';
 import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <Banner></Banner>
     </div>
   );
 }
