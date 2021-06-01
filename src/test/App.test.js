7faf21560b0ecc1e5a05ac4310aa9d9193ec4ae6
KhoaDang <dangkhoa1410@gmail.com>
2021-06-01 08:50:16 -0400

    Added App test to test all components rendered

---
+++ src/test/App.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import {BrowserRouter, Link} from 'react-router-dom'
+
+import App from '../App'
+import Projects from "../components/Projects/Projects";
+import Nav from "../components/Nav/Nav";
+import { projects } from "../utils/Jquery";
+import Banner from "../components/Banner/Banner";
+import About from "../components/About/About";
+import Summary from "../components/Summary/Summary";
+import Footer from "../components/Footer/Footer";
+import Contact from "../components/Contact/Contact";
+
+describe('App test', () => {
+    it('correctly renders all components', () => {
+        const wrapper = shallow(<App/>)
+        expect(wrapper.containsAllMatchingElements([Nav,Banner,About,Summary,Projects,Contact,Footer]))
+    })
+
+
+})
