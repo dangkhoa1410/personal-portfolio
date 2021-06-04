932914c3d89721bf4687588b33334a341daa14c9
KhoaDang <dangkhoa1410@gmail.com>
2021-06-04 18:57:37 -0400

    Added a seperate Info file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,29 +3,11 @@ import '../static/CSS/Projects.css'
 import {projects} from '../utils/Jquery/index'
 import { useWindowSize } from '../utils/Hooks/useWindowSize'
 import withStyling from '../HOC/withResizing'
+import Info from './Info'
 
 const Projects = props => {
 	//Add project info here
-	const projectInfo = [
-		{
-			className: 'luvtalk-project',
-			h3: 'LUV TALK Website',
-			p: 'Website, created during my first internship, which utilized HTML, CSS, PHP, Ajax, Javascript/jQuery, and Wordpress.',
-			link: '#'
-		},
-		{
-			className: 'personal-website-project',
-			h3: 'Personal Website',
-			p: 'Enjoyable side project that was created to experiment with more HTML and CSS, but also provided an outlet to showcase my abilities and interests.',
-			link: '#'
-		},
-		{
-			className: 'strike-zone-project',
-			h3: 'Strike Zone Analysis',
-			p: 'Data analytics project completed during my time at the Illinois Math and Science Academy which studied the baseball strike zone based on the state of the game.',
-			link: '#'
-		}
-]
+	const projectInfo = Info.projectInfo
 
     return (
         <div className = "projects" id="projects">
